fd13cfc9dcf419f14ea8c5cfac884535fc8f861c
Matthew-Tian5 <matthew.tian2005@gmail.com>
2025-09-24 15:20:02 -0400

    updated the download path to user's selection

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ import os
 from tkinter import filedialog
 import tkinter as tk
 
+def download_path():
+    root = tk.Tk()
+    root.withdraw() 
+    folder_selected = filedialog.askdirectory()
+    return folder_selected if folder_path else os.getcwd()
+
 a = ""
 
 while (a !="QUIT"):
 
     a = input("Enter the YouTube video URL: ")
     try:
+        if(a!="QUIT"):
+            
+            yt = YouTube(a)
 
-        yt = YouTube(a)
+            video_stream = yt.streams.get_highest_resolution()
 
-        video_stream = yt.streams.get_highest_resolution()
-
-        print(f"Downloading: {yt.title}")
-        video_stream.download()
-        print("Download complete!")
+            print(f"Downloading: {yt.title}")
+            video_stream.download()
+            print("Download complete!")
 
     except Exception as e:
         error_message = handle_download_error(e)
